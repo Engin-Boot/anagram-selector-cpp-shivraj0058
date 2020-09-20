17d98aaed6dd9d6c3154759e6389ed5a8698d7e1
shivaraja s <shivaraj20054@gmail.com>
2020-09-20 16:20:50 +0530

    anagram

--- anagram.cpp
+++ anagram.cpp
@@ -6,24 +6,33 @@
 using namespace std;
 
 bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2) {
-    string s1 = word1;
-    string s2 = word2;
-    s1.erase(remove_if(s1.begin(), s1.end(),
-        [](char c) { return !isalpha(c); }), s1.end());
-
-    s2.erase(remove_if(s2.begin(), s2.end(),
-        [](char c) { return !isalpha(c); }), s2.end());
+    string s1 = EraseSpace(word1);
+    string s2 = EraseSpace(word2);
     if (s1.length() != s2.length())
     {
         return false;
     }
-    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
-    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end());
+    TransformString(s1);
+    TransformString(s2);
     if (s1 == s2)
         return true;
     return false;
+}
+
+std::string Anagram::TransformString(string word)
+{
+    std::string str=word;
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    sort(str.begin(), str.end());
+    return str;
+}
+
+std::string Anagram::EraseSpace(std::string word)
+{
+    std::string str=word;
+    str.erase(remove_if(str.begin(), str.end(),
+        [](char c) { return !isalpha(c); }), str.end());
+    return str;
 }
 
 std::vector<std::string> Anagram::SelectAnagrams(
--- anagram.h
+++ anagram.h
@@ -4,6 +4,8 @@
 namespace Anagram
 {
     bool WordPairIsAnagram(const std::string& word1, const std::string& word2);
+    std::string TransformString(std::string);
+    std::string EraseSpace(std::string);
 
     std::vector<std::string> SelectAnagrams(
         const std::string& word,
